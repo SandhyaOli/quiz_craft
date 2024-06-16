d10d93aa6d3d08b92b0e8e1f422691da8faecd06
sandhyaoli <olisandhya707@gmail.com>
2024-06-16 17:45:54 +0545

    seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             QuestionSeeder::class,
             StudentSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
